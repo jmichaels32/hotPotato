40b70fa0edc6bb5dca771b9007930b233cc99f22
Jack Michaels <jackfm@stanford.edu>
2023-02-16 16:11:31 -0800

    Revert "Added constants for page management"
    This reverts commit 948567ae78d32515012072b7104d52f1bb3fc6be.

--- HotPotato/App.js
+++ HotPotato/App.js
@@ -1,17 +1,16 @@
 import { Component } from 'react'
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import * as Constants from './src/constants.js'
 
 class App extends Component {
   state = {
-    page: Constants.WORKOUTPAGE,
+    
   };
 
   render() {
     return (
       <View style={styles.container}>
-        <Text>You're current on page number: {this.state.page} </Text>
+        <Text>Open up App.js to start working on your app!</Text>
         <StatusBar style="auto" />
       </View>
     );
--- HotPotato/src/constants.js
+++
@@ -1,5 +0,0 @@
-// Page Constants
-export const WORKOUTPAGE = 0;
-export const FORTRESSPAGE = 1;
-export const BATTLEPAGE = 2;
-export const PROFILEPAGE = 3;
