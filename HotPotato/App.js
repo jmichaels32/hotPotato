948567ae78d32515012072b7104d52f1bb3fc6be
Jack Michaels <jackfm@stanford.edu>
2023-02-16 16:07:22 -0800

    Added constants for page management

--- HotPotato/App.js
+++ HotPotato/App.js
@@ -1,16 +1,17 @@
 import { Component } from 'react'
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Constants from './src/constants.js'
 
 class App extends Component {
   state = {
-    
+    page: Constants.WORKOUTPAGE,
   };
 
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>You're current on page number: {this.state.page} </Text>
         <StatusBar style="auto" />
       </View>
     );
---
+++ HotPotato/src/constants.js
@@ -0,0 +1,5 @@
+// Page Constants
+export const WORKOUTPAGE = 0;
+export const FORTRESSPAGE = 1;
+export const BATTLEPAGE = 2;
+export const PROFILEPAGE = 3;
