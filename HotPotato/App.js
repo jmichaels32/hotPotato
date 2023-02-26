3a5e1f3d6530704fcc9c26666a88c18b40b16e51
Jack Michaels <jackfm@stanford.edu>
2023-02-26 12:18:07 -0800

    Reorganized the main layout of the app

--- HotPotato/App.js
+++ HotPotato/App.js
@@ -6,19 +6,29 @@ import { StyleSheet, TouchableOpacity, Image, Text, View } from 'react-native';
 import Pages from './pages.js';
 import * as Const from './constants.js';
 
+const TopBar = (props) => {
+  return (
+    <View style={styles.topbar}>
+      <TouchableOpacity>
+        <Image style={{width: 30, height: 30, marginBottom: 5, marginLeft: 13}} source={Const.backButtonPath}/>
+      </TouchableOpacity>
+      <Image style={{width: '29%', height: '35.1%'}} source={Const.logoPath} />
+      <TouchableOpacity>
+        <Image style={{width: 40, height: 40}} source={Const.activityPlusPath}/>
+      </TouchableOpacity>
+    </View>
+  )
+}
+
 class App extends Component {
   state = {
     currentPage : Const.WORKOUTPAGE,
   };
 
-  /*
-  onPress(state) {
-    this.setState()
-  }*/
-
   render() {
     return (
       <View style={styles.container}>
+        <TopBar />
         <Pages currentPage={this.state.currentPage}/>
         <View style={styles.navbar}>
           <TouchableOpacity onPress={() => this.setState({currentPage : Const.WORKOUTPAGE})} title="Workout" color="#D6AE60">
@@ -41,16 +51,24 @@ class App extends Component {
 }
 
 const styles = StyleSheet.create({
-  icons: {
-    resizeMode: 'contain',
-    height: 65,
-    width: 65,
-  },
   container: {
     flex: 1,
     backgroundColor: '#D6AE60',
     justifyContent: 'center',
   }, 
+  topbar: {
+    backgroundColor: '#FDDC9B',
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+    justifyContent: 'space-around',
+    height: '12.5%',
+    paddingBottom: 3,
+  },
+  icons: {
+    resizeMode: 'contain',
+    height: 65,
+    width: 65,
+  },
   navbar: {
     display: 'flex',
     backgroundColor: '#FDDC9B',
@@ -58,13 +76,18 @@ const styles = StyleSheet.create({
     justifyContent: 'space-evenly',
     alignItems: 'center',
     position: 'absolute',
-    left: '2%',
     bottom: -30,
-    height: 110,
-    width: '96%',
+    height: '12.5%',
     marginBottom: 20,
     paddingBottom: 25,
-    borderRadius: 5,
+    
+    // To make the navbar like the figma
+    //left: '2%',
+    //width: '96%',
+    //borderRadius: 5,
+
+    // An alternate design
+    width: '100%',
   }
 });
 
--- HotPotato/constants.js
+++ HotPotato/constants.js
@@ -11,11 +11,18 @@ export const PROFILEPAGE = 3;
 // Paths for images
 // ----------------------------------
 
+// Top Bar Images
+export const logoPath = require('./images/topbar/logo.png');
+export const backButtonPath = require('./images/topbar/backButton.png');
+export const activityPlusPath = require('./images/topbar/activityPlus.png');
+
+// Unselected Navigation Bar Images
 export const workoutIconPath = require('./images/icons/workoutIcon.png');
 export const fortressIconPath = require('./images/icons/fortressIcon.png');
 export const battleIconPath = require('./images/icons/battleIcon.png');
 export const profileIconPath = require('./images/icons/profileIcon.png');
 
+// Selected Navigation Bar Images
 export const selectedWorkoutIconPath = require('./images/selectedIcons/selectedWorkoutIcon.png');
 export const selectedFortressIconPath = require('./images/selectedIcons/selectedFortressIcon.png');
 export const selectedBattleIconPath = require('./images/selectedIcons/selectedBattleIcon.png');
