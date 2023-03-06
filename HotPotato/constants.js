05f07c702cd55c8568aacecdca4b9e2f99e8bf50
Jack Michaels <jackfm@stanford.edu>
2023-03-05 21:20:09 -0800

    Small simplification

--- HotPotato/constants.js
+++ HotPotato/constants.js
@@ -2,7 +2,6 @@
 // File defining constants for widespread use within the app
 // ----------------------------------
 
-export const NOPAGE = -1;
 export const WORKOUTPAGE = 0;
 export const FORTRESSPAGE = 1;
 export const BATTLEPAGE = 2;
